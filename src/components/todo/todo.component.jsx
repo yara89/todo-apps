202a202bf85098c0cf333c8650e38ea8ad04de48
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-17 16:31:42 +0100

    Update todo.component.jsx

--- src/components/todo/todo.component.jsx
+++ src/components/todo/todo.component.jsx
@@ -6,7 +6,7 @@ export function Todo(props) {
     return (<div className="list-item">
                {props.task}
                {props.index}
-               {props.title}
+               {props.task.title}
 
 
     <button className="delete is-pulled-right" onClick = {() => {props.onDelete(props.index)}}> </button>
